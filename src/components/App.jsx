d57b86c6c5c50db63ddced39b64559d309340f7c
Ed <silva.email@yahoo.com>
2024-03-06 18:17:23 -0500

    Add state and event handler for input field

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+const [item, setItem] = useState("")
+
+function handleChange(e){
+  const [name, value] = e.target
+  setItem((prevChange) =>{
+
+  })
+}
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" placeholder="Enter item" onChange={handleChange} value={item} />
+        <button >
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          <li>{item}</li>
         </ul>
       </div>
     </div>
